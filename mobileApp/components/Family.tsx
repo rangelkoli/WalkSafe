e327e969b53d098df36fe2b7b93c0412d96d35e1
Rangel Koli <rangelkoli@gmail.com>
2024-03-10 13:09:01 -0400

    Added Bottom Tabs Navigation Icons

--- mobileApp/components/Family.tsx
+++ mobileApp/components/Family.tsx
@@ -1,11 +1,18 @@
-import { StyleSheet, Text, View } from 'react-native'
+import { StyleSheet, Text, View, SafeAreaView, Image } from 'react-native'
 import React from 'react'
 
 const Family = () => {
   return (
-    <View>
+    <SafeAreaView>
       <Text>Family</Text>
-    </View>
+        <Image
+            style={{ width: 50, height: 50, backgroundColor: 'red'}}
+            source={require('./home.svg')}
+            width={20}
+            height={20}
+
+        />
+    </SafeAreaView>
   )
 }
 
--- mobileApp/components/navigation/navigation.tsx
+++ mobileApp/components/navigation/navigation.tsx
@@ -7,6 +7,9 @@ import { Session } from '@supabase/supabase-js';
 import Account from '../Account';
 import AddAlert from '../AddAlert';
 import Family from '../Family';
+import { Image, View, Platform } from 'react-native';
+
+
 
 const Tab = createMaterialBottomTabNavigator();
 
@@ -14,42 +17,124 @@ export default function BottomNavigation(
     { session }: { session: Session }
 ) {
   return (
-    <NavigationContainer>
+    <NavigationContainer
+    >
         <Tab.Navigator 
         initialRouteName='Home' 
         activeColor="black" 
         inactiveColor="gray" 
         style={{
-            backgroundColor: 'blue',
             margin: 0,
             padding: 0,
+            borderRadius: 0,
+            backgroundColor: 'white',
         }}
-        
+        screenOptions={{
+            tabBarColor: 'blue',
+        }}
+        barStyle={{
+            backgroundColor: 'white',
+            padding: 0,
+            margin: 0,  
+            borderRadius: 0,
+        }}
         >
         <Tab.Screen 
             name="Home" 
             component={Maps} 
             options={{
                 tabBarLabel: 'Home',
-                tabBarColor: 'blue',
-                
-
+                tabBarColor: 'red',
+                tabBarIcon: ({ color }) => {
+                    return (
+                        <View
+                            style={{ 
+                                justifyContent: 'center',
+                                alignItems: 'center',
+                            }}
+                        >
+                            <Image
+                                source={require('./home.png')}
+                                style={{ width: 30, height: 30}}
+                            />
+                        </View>
+                    )
+                }  
             }} 
-            
             />
-
         <Tab.Screen
             name="Family"
             component={Family}
             options={{
                 tabBarLabel: 'Family',
+                tabBarIcon: ({ color }) => {
+                    return (
+                        <View
+                            style={{ 
+                                justifyContent: 'center',
+                                alignItems: 'center',
+                            }}
+                        >
+                            <Image
+                                source={require('./family.png')}
+                                style={{ width: 30, height: 30}}
+                            />
+                        </View>
+                    )
+                }  
             }}
+
         />
                 <Tab.Screen
             name="Alerts"
             component={AddAlert}
             options={{
                 tabBarLabel: 'Alerts',
+                // tabBarIcon: ({ color }) => {
+                //     return (
+                //         <View
+                //             style={{ 
+                //                 width: 30, 
+                //                 height: 30,
+                //                 top: Platform.OS === 'ios' ? -30 : -20,
+                //                 minWidth: Platform.OS === 'ios' ? 50 : 60,
+                //                 minHeight: Platform.OS === 'ios' ? 50 : 60,
+                //                 borderRadius: 50,
+                //                 backgroundColor: color,
+                //                 justifyContent: 'center',
+                //                 alignItems: 'center',
+                //                 shadowColor: 'black',
+                //                 }}
+                //         >
+                //             <Image
+                //                 source={require('./home.svg')}
+                //                 style={{ width: 20, height: 20, zIndex: 10}}
+                //                 width={20}
+                //                 height={20}
+                //             />
+                //         </View>
+                //     )
+                // }  
+                tabBarIcon: ({ color }) => {
+                    return (
+                        <View
+                            style={{ 
+                                justifyContent: 'center',
+                                alignItems: 'center',
+                            }}
+                        >
+                            <Image
+                                source={require('./alerts.png')}
+                                style={{ 
+                                    width: 30, 
+                                    height: 30,
+
+                                }}
+                            />
+                        </View>
+                    )
+                }  
+
             }}
         />
                 <Tab.Screen
@@ -57,6 +142,21 @@ export default function BottomNavigation(
             component={Test}
             options={{
                 tabBarLabel: 'Family',
+                tabBarIcon: ({ color }) => {
+                    return (
+                        <View
+                            style={{ 
+                                justifyContent: 'center',
+                                alignItems: 'center',
+                            }}
+                        >
+                            <Image
+                                source={require('./home.png')}
+                                style={{ width: 30, height: 30}}
+                            />
+                        </View>
+                    )
+                }  
             }}
         />
         <Tab.Screen
@@ -64,6 +164,23 @@ export default function BottomNavigation(
             children={()=><Account session={session} />}
             options={{
                 tabBarLabel: 'Account',
+                tabBarBadge: 3,
+                tabBarColor: 'red',
+                tabBarIcon: ({ color }) => {
+                    return (
+                        <View
+                            style={{ 
+                                justifyContent: 'center',
+                                alignItems: 'center',
+                            }}
+                        >
+                            <Image
+                                source={require('./profile.png')}
+                                style={{ width: 30, height: 30}}
+                            />
+                        </View>
+                    )
+                }  
             }}
         />
 
